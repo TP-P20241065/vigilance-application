38c11a472466cd2e7a31097cc8a5362a3d7d0a5c
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2024-10-11 04:38:39 -0500

    Fix: OpenCV installation

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 if exist "dist" rd /q /s "dist"
-pyinstaller.exe main.py --add-binary "<TU RUTA>\Python\Python310\Lib\site-packages\cv2\opencv_videoio_ffmpeg480_64.dll;." --add-data=".env;." --collect-data=ultralytics
+pyinstaller --noconsole --hiddenimport=requests --hiddenimport=opencv-python --collect-data=ultralytics main.py
 copy "alert.wav" "dist/main/_internal"
-copy "best.pt" "dist/main"
+copy "best_V2.pt" "dist/main"
 copy "metropolitano-lima-bus-41.jpg" "dist/main/_internal"
 copy "ZuriCam.ui" "dist/main/_internal"
+rename "dist\main\main.exe" "ZuriCam.exe"
